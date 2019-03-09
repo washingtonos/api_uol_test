cbca1975f061e9829ed50ffa4ed0266f3b42852c
Washington <washingtonoliveira441@gmail.com>
2019-03-09 00:28:55 -0300

    Criacao do projeto e sua estrutura

---
+++ uol/src/test/java/br/com/washington/uol/uol/UolApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.washington.uol.uol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
